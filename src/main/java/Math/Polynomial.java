07b980d762eef08fac691697df5544d9c23543cd
alim <alim.filipov@gmail.com>
2021-04-22 18:35:14 +0300

    First Commit

---
+++ src/main/java/Math/Polynomial.java
@@ -0,0 +1,176 @@
+package Math;
+
+import org.scilab.forge.jlatexmath.TeXConstants;
+import org.scilab.forge.jlatexmath.TeXFormula;
+
+import java.awt.*;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.List;
+
+public class Polynomial {
+
+    public static Map<Double, Double> findPolynomialByPoints(List<Double> x, List<Double> fx) {
+        int size = x.size();
+        assert (size != fx.size());
+
+        List<Map<Double, Double>> maps = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+
+            List<Double> xCopy = copy(x, i);
+            //works with coefficients
+            double under = 1.0;
+            for (int j = 0; j < size; j++) {
+                if (j != i) {
+                    under *= (x.get(i) - x.get(j));
+                }
+            }
+            double coefficient = fx.get(i) / under;
+
+            Map<Double, Double> polynomial = multiply(createPolynomial(xCopy));
+            maps.add(multiply(polynomial, coefficient));
+        }
+        return sum(maps);
+    }
+
+    private static List<Double> copy(List<Double> list, int exceptIndex) {
+        List<Double> res = new ArrayList<>(list.size() - 1);
+        for (int i = 0; i < list.size(); i++) {
+            if (i != exceptIndex) {
+                res.add(list.get(i));
+            }
+        }
+        return res;
+    }
+
+    public static List<Map<Double, Double>> createPolynomial(List<Double> x) {
+        List<Map<Double, Double>> mapList = new ArrayList<>();
+
+        for(Double c : x) {
+            Map<Double, Double> temp = new TreeMap<>();
+
+            //put x : (x - c)
+            temp.put(1.0, 1.0);
+            //put c : (x - c)
+            temp.put(0.0, -c);
+
+            mapList.add(temp);
+        }
+        return mapList;
+    }
+    /*
+     * Раскроем скобки:
+     * (x^2+3x+1)(x^3-2x-1)
+     * x^2(x^3-2x-1) + 3x(x^3-2x-1) + 1(x^3-2x-1)
+     * Функция выполняет умножения, затем передаёт результат в ф-цию sum(...), чтоб упростить рез-т
+     * В данном случае, передаём эти три выражения в массиве, потом возвращаем рез-т
+     */
+    public static Map<Double, Double> multiply(Map<Double, Double> first, Map<Double, Double> second) {
+
+        List<Map<Double, Double>> result = new ArrayList<>();
+
+        for (Map.Entry<Double, Double> f : first.entrySet()) {
+
+            Map<Double, Double> multiplyRes = new HashMap<>();
+
+            for (Map.Entry<Double, Double> s : second.entrySet()) {
+                //степени складываются, коэффициенты умножаются
+                double pow = f.getKey() + s.getKey();
+                double k = f.getValue() * s.getValue();
+                multiplyRes.put(pow, k);
+            }
+
+            result.add(multiplyRes);
+        }
+        return sum(result);
+    }
+
+    public static Map<Double, Double> multiply(List<Map<Double, Double>> mapList) {
+
+        assert !mapList.isEmpty();
+
+        while(mapList.size() > 1) {
+            Map<Double, Double> temp =  multiply(mapList.get(1), mapList.get(0));
+
+            mapList.remove(0);
+            mapList.remove(0);
+            mapList.add(temp);
+        }
+
+        return mapList.get(0);
+    }
+
+    public static Map<Double, Double> multiply(Map<Double, Double> map, double num) {
+        map.replaceAll((k, v) -> v * num);
+        return map;
+    }
+
+        /*
+     * Суммирует выражения из многочлена:
+     * (x^5 - 2x^3 - x^2) + (3x^4 - 6x^2 - 3x) + (x^3 - 2x - 1)
+     * В реализации используется словарь с сортировкой
+     */
+    public static Map<Double, Double> sum(List<Map<Double, Double>> polynomial) {
+        Map<Double, Double> res = new TreeMap<>(Collections.reverseOrder());
+
+        for (Map<Double, Double> l : polynomial) {
+            for (Map.Entry<Double, Double> pair : l.entrySet()) {
+
+                Double pairKey = pair.getKey();
+
+                if (res.containsKey(pairKey)) {
+                    res.put(pairKey, res.get(pairKey) + pair.getValue());
+                } else {
+                    res.put(pairKey, pair.getValue());
+                }
+            }
+        }
+        return res;
+    }
+
+
+    /*
+                В Java для часто меняющихся строковых переменных используют StringBuilder.
+                функция append() всего лишь добавляет строку в конец
+                далее использую RegEx, чтоб привести строку в приятный математику вид
+        */
+
+    public static String toString(Map<Double, Double> map) {
+        DecimalFormat df = new DecimalFormat("#.###");
+        StringBuilder s = new StringBuilder();
+
+        for (Map.Entry<Double, Double> p : map.entrySet()) {
+            double key = p.getKey();
+            double value = p.getValue();
+
+            s.append(df.format(value)).append("x^").append(key).append("+");
+        }
+        s.insert(0, "L(x) = ");
+
+        //удаляем лишние символы
+        s.deleteCharAt(s.length() - 1);
+
+        return s.toString()
+                .replaceAll("\\+-", "-")
+                .replaceAll("\\.999", "")
+                .replaceAll("\\.0", "")
+                .replaceAll("\\^1", "")
+                .replaceAll("x\\^0", "")
+                .replaceAll("1x", "x");
+    }
+
+
+    /*
+        Выдаёт файл с формулой через LaTex в PNG формате
+     */
+    public static void printLatex(String latex) {
+        TeXFormula formula = new TeXFormula(latex);
+        formula.createPNG(TeXConstants.STYLE_DISPLAY, 20, "target/ResultLatex.png", Color.WHITE, Color.BLACK);
+    }
+
+    public static void printLatex(String latex, String folderPath) {
+        TeXFormula formula = new TeXFormula(latex);
+        formula.createPNG(TeXConstants.STYLE_DISPLAY, 20, folderPath, Color.WHITE, Color.BLACK);
+    }
+}
---
+++ src/test/java/PolynomialTest.java
@@ -0,0 +1,168 @@
+import Math.Polynomial;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class PolynomialTest {
+    static Map<Double, Double> p1 = new TreeMap<>();
+    static Map<Double, Double> p2 = new TreeMap<>();
+    static Map<Double, Double> p3 = new TreeMap<>();
+    static Map<Double, Double> p4 = new TreeMap<>();
+
+    /*
+        initialize variables
+     */
+    static {
+        // (x^5 - 2x^3 - x^2)
+        p1.put(5.0, 1.0);
+        p1.put(3.0, -2.0);
+        p1.put(2.0, -1.0);
+
+        // (3x^4 - 6x^2 - 3x)
+        p2.put(4.0, 3.0);
+        p2.put(2.0, -6.0);
+        p2.put(1.0, -3.0);
+
+        // (x^3 - 2x - 1)
+        p3.put(3.0, 1.0);
+        p3.put(1.0, -2.0);
+        p3.put(0.0, -1.0);
+
+        // (x^0+1x+x^2)
+        p4.put(0.0, 1.0);
+        p4.put(1.0, 1.0);
+        p4.put(2.0, 1.0);
+    }
+
+    public static void main(String[] args) {
+        List<Double> x = Arrays.asList( 1.0,    -5.0,    -3.0,   -2.0,   -1.0);
+        List<Double> y = Arrays.asList(10.0,	40.0,   -34.0,	-11.0,	  4.0);
+
+        Map<Double, Double> res = Polynomial.findPolynomialByPoints(x, y);
+        Polynomial.printLatex(Polynomial.toString(res));
+    }
+
+    @Test
+    void multiply() {
+        Map<Double, Double> p = Polynomial.multiply(p2, p3);
+
+        //pow
+        Assertions.assertTrue(p.containsKey(7.0));
+        Assertions.assertTrue(p.containsKey(5.0));
+        Assertions.assertTrue(p.containsKey(4.0));
+        Assertions.assertTrue(p.containsKey(3.0));
+        Assertions.assertTrue(p.containsKey(2.0));
+        Assertions.assertTrue(p.containsKey(1.0));
+
+        Assertions.assertFalse(p.containsKey(6.0));
+        Assertions.assertFalse(p.containsKey(0.0));
+
+        //coefficient
+        Assertions.assertEquals(3.0, p.get(7.0));
+        Assertions.assertEquals(-12.0, p.get(5.0));
+        Assertions.assertEquals(-6.0, p.get(4.0));
+        Assertions.assertEquals(12.0, p.get(3.0));
+        Assertions.assertEquals(12.0, p.get(2.0));
+        Assertions.assertEquals(3.0, p.get(1.0));
+    }
+
+    @Test
+    void multiplyNum() {
+        Map<Double, Double> map = new HashMap<>();
+        // 3(x^2+2x-11)
+        map.put(2.0, 1.0);
+        map.put(1.0, 2.0);
+        map.put(0.0, -11.0);
+
+        Polynomial.multiply(map, 3.0);
+
+        Assertions.assertEquals(3, map.size());
+
+        Assertions.assertEquals(3.0, map.get(2.0));
+        Assertions.assertEquals(6.0, map.get(1.0));
+        Assertions.assertEquals(-33.0, map.get(0.0));
+
+    }
+
+    @Test
+    void multiplyListMap() {
+        //(x-1)
+        Map<Double, Double> polynomial1 = new HashMap<>();
+        polynomial1.put(1.0, 1.0);
+        polynomial1.put(0.0, -1.0);
+
+        //(x-1)
+        Map<Double, Double> polynomial2 = new HashMap<>();
+        polynomial2.put(1.0, 1.0);
+        polynomial2.put(0.0, -1.0);
+
+        //(x-1)
+        Map<Double, Double> polynomial3 = new HashMap<>();
+        polynomial3.put(1.0, 1.0);
+        polynomial3.put(0.0, -1.0);
+
+        //(x-1)(x-1)(x-1)
+        List<Map<Double, Double>> mapList = new ArrayList<>();
+        mapList.add(polynomial1);
+        mapList.add(polynomial2);
+        mapList.add(polynomial3);
+
+        Map<Double, Double> res = Polynomial.multiply(mapList);
+
+        // we must have: (x-1)^3 = x^3 - 3x^2 + 3x - 1
+        Assertions.assertEquals(1.0, res.get(3.0));
+        Assertions.assertEquals(-3.0, res.get(2.0));
+        Assertions.assertEquals(3.0, res.get(1.0));
+        Assertions.assertEquals(-1.0, res.get(0.0));
+    }
+
+    @Test
+    void sum() {
+        List<Map<Double, Double>> testV = new ArrayList<>();
+        testV.add(p1);
+        testV.add(p2);
+        testV.add(p3);
+
+        Map<Double, Double> p = Polynomial.sum(testV);
+        //  check if result:
+        //  x^5 + 3x^4 - x^3 - 7x^2 - 5x - 1
+        Assertions.assertEquals(1.0, p.get(5.0));
+        Assertions.assertEquals(3.0, p.get(4.0));
+        Assertions.assertEquals(-1.0, p.get(3.0));
+        Assertions.assertEquals(-7.0, p.get(2.0));
+        Assertions.assertEquals(-5.0, p.get(1.0));
+        Assertions.assertEquals(-1.0, p.get(0.0));
+
+        Polynomial.printLatex(Polynomial.toString(p));
+    }
+
+    @Test
+    void createPolynomial() {
+        List<Double> x = Arrays.asList(1.0, 2.0, -3.0);
+        List<Map<Double, Double>> mapList = Polynomial.createPolynomial(x);
+
+        Assertions.assertEquals(3, mapList.size());
+
+        Assertions.assertEquals(-1.0, mapList.get(0).get(0.0));
+        Assertions.assertEquals(-2.0, mapList.get(1).get(0.0));
+        Assertions.assertEquals( 3.0, mapList.get(2).get(0.0));
+    }
+
+    /*
+            toString p4 : (x^0+1x+x^2) like 1+x+x^2
+         */
+    @Test
+    void print() {
+        String expResult = "f(x) = 1+x+x^2";
+        Assertions.assertEquals(expResult, Polynomial.toString(p4));
+
+        Polynomial.printLatex(Polynomial.toString(p4));
+    }
+
+    //todo: write normal tests for latex
+//    @Test
+//    void printLatex() {
+//        Polynomial.printLatex("hi", "target/testHI.png");
+//    }
+}
